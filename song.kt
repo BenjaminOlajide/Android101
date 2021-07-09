105fa2a127d2a8d143195a518fbb9bef826ef13d
Benjamin Olajide <40693259+BenjaminOlajide@users.noreply.github.com>
2021-07-09 14:25:10 -0500

    Songs.kt Variables

---
+++ song.kt
@@ -0,0 +1,14 @@
+/**************************************************************
+* File Title = SONG                                           *
+* This file is for gathering song attributes into appropriate *
+* variables suitable for each data type                       *
+**************************************************************/
+
+val songTitle: String = "God will make a way" //This is the song title
+val sondDuration: Float = 4.00f //Song play time in minutes
+val songArtist: String = "Don Moen" //The name of the recording artist
+val releaseYear: Int = 2003 //This is the year the song was officially released
+val recordLabel: String = "Integrity Music" //The record label that released the song
+var onSpotify: Boolean = true //This is to confirm if the song is on Spotify
+val genre: String = "Gospel" //The genre of the music
+val albumTitle: String = "The Best of Don Moen" //Title of the album the song was released on
